6ebcd06754d29d2c5d3d0fb549cb8600ecbe2e2d
samvelha <hambardzumyansamvel90@gmail.com>
2020-07-09 20:24:10 +0400

    task 2

---
+++ javascript1.js
@@ -0,0 +1,22 @@
+let a = 5;
+let b = 78;
+
+let sum = a + b;
+console.log(sum);
+alert(sum)
+
+let sub = a - b;
+console.log(sub);
+alert(sub)
+
+let mult = a * b;
+console.log(mult);
+alert(mult)
+
+let divs = a / b;
+console.log(divs);
+alert(divs)
+
+let rem = a % b;
+console.log(rem);
+alert(rem)
